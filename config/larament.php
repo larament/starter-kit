b0e7e77928984165ddd10e56ace99a1e6f3ee39c
Raziul Islam <raziul.cse@gmail.com>
2024-10-10 18:47:48 +0600

    larament config

---
+++ config/larament.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'super_admin' => [
+        'email' => env('LARAMENT_SUPER_ADMIN_EMAIL', 'super-admin@mail.com'),
+        'password' => env('LARAMENT_SUPER_ADMIN_PASSWORD', 'password'),
+    ],
+];
