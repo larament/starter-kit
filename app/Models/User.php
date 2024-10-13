e5da9896989df7df543eeb0c373b1b34d2e91e55
iRaziul <raziul.cse@gmail.com>
2024-10-13 11:37:00 +0600

    soft delete user

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,16 +3,21 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory;
     use Notifiable;
     use HasRoles;
+    use SoftDeletes;
 
     protected $fillable = [
         'name',
@@ -34,5 +39,11 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        // TODO
+        return true;
     }
 }
